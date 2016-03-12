59b463b1405cb731568e6cd12249b79d877d53bf
Jackie Ho <JHADI@Jackies-MacBook-Pro.local>
2016-03-12 11:04:58 -0500

    first commit

---
+++ app/src/main/java/com/adi/ho/jackie/pricecomparisonapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adi.ho.jackie.pricecomparisonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String API_KEY = "f3c5459e-f77d-40f6-b53f-43154e4559f9";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

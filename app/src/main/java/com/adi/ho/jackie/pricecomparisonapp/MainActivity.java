4e5b8f82cf40310496fd213dbc4ada76745d066c
Jackie Ho <JHADI@Jackies-MacBook-Pro.local>
2016-03-12 13:37:16 -0500

    image code added with HODClient

--- app/src/main/java/com/adi/ho/jackie/pricecomparisonapp/MainActivity.java
+++ app/src/main/java/com/adi/ho/jackie/pricecomparisonapp/MainActivity.java
@@ -1,15 +1,205 @@
 package com.adi.ho.jackie.pricecomparisonapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import hod.api.hodclient.HODApps;
+import hod.api.hodclient.HODClient;
+import hod.api.hodclient.IHODClientCallback;
+import hod.response.parser.BarcodeRecognitionResponse;
+import hod.response.parser.HODResponseParser;
+
+public class MainActivity extends AppCompatActivity implements IHODClientCallback {
+    private static final int SELECT_PICTURE = 1;
+    private static final int TAKE_PICTURE = 2;
+    private String mImageFullPathAndName = "";
+    private String localImagePath = "";
+    private static final int OPTIMIZED_LENGTH = 1024;
+
 
     private static final String API_KEY = "f3c5459e-f77d-40f6-b53f-43154e4559f9";
-
+    HODClient hodClient = new HODClient(API_KEY, this);
+    HODResponseParser parser = new HODResponseParser();
+    String hodApp = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CreateLocalImageFolder();
+    }
+
+    @Override
+    public void requestCompletedWithContent(String response) {
+        if (hodApp.equals(HODApps.RECOGNIZE_BARCODES)) {
+            BarcodeRecognitionResponse resp = parser.ParseBarcodeRecognitionResponse(response);
+            if (resp != null) {
+                String test = resp.barcode.get(0).text;
+            }
+        }
+    }
+
+    @Override
+    public void requestCompletedWithJobID(String response) {
+        String jobID = parser.ParseJobID(response);
+        if (jobID.length() > 0)
+            hodClient.GetJobResult(jobID);
+    }
+
+    @Override
+    public void onErrorOccurred(String errorMessage) {
+
+    }
+
+    // loading image part
+    private void callHODAPI() {
+        hodApp = HODApps.RECOGNIZE_BARCODES;
+        Map<String,Object> params = new HashMap<>();
+        params.put("file", mImageFullPathAndName);
+        hodClient.PostRequest(params, hodApp, HODClient.REQ_MODE.ASYNC);
+    }
+    public void CreateLocalImageFolder()
+    {
+        if (localImagePath.length() == 0)
+        {
+            localImagePath = getFilesDir().getAbsolutePath() + "/orc/";
+            File folder = new File(localImagePath);
+            boolean success = true;
+            if (!folder.exists()) {
+                success = folder.mkdir();
+            }
+            if (!success)
+                Toast.makeText(this, "Cannot create local folder", Toast.LENGTH_LONG).show();
+        }
+    }
+    public Bitmap decodeFile(File file) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = 1;
+        int mImageRealWidth = options.outWidth;
+        int mImageRealHeight = options.outHeight;
+        Bitmap pic = null;
+        try {
+            pic = BitmapFactory.decodeFile(file.getPath(), options);
+        } catch (Exception ex) {
+            Log.e("MainActivity", ex.getMessage());
+        }
+        return pic;
+    }
+    public Bitmap rescaleBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
+    }
+    private Bitmap rotateBitmap(Bitmap pic, int deg) {
+        Matrix rotate90DegAntiClock = new Matrix();
+        rotate90DegAntiClock.preRotate(deg);
+        Bitmap newPic = Bitmap.createBitmap(pic, 0, 0, pic.getWidth(), pic.getHeight(), rotate90DegAntiClock, true);
+        return newPic;
+    }
+
+    private String SaveImage(Bitmap image)
+    {
+        String fileName = localImagePath + "imagetoocr.jpg";
+        try {
+
+            File file = new File(fileName);
+            FileOutputStream fileStream = new FileOutputStream(file);
+
+            image.compress(Bitmap.CompressFormat.JPEG, 100, fileStream);
+            try {
+                fileStream.flush();
+                fileStream.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+    public void DoTakePhoto(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    public void DoShowSelectImage(View v) {
+        Intent i = new Intent(
+                Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, SELECT_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_PICTURE || requestCode == TAKE_PICTURE) {
+            if (resultCode == RESULT_OK && null != data) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                mImageFullPathAndName = cursor.getString(columnIndex);
+                cursor.close();
+                File file = new File(mImageFullPathAndName);
+                Bitmap mCurrentSelectedBitmap = decodeFile(file);
+
+                if (mCurrentSelectedBitmap != null) {
+                    // display the full size image
+                    //ivSelectedImg.setImageBitmap(mCurrentSelectedBitmap);
+                    // scale the image
+                    // let check the resolution of the image.
+                    // If it's too large, we can optimize it
+                    int w = mCurrentSelectedBitmap.getWidth();
+                    int h = mCurrentSelectedBitmap.getHeight();
+
+                    int length = (w > h) ? w : h;
+                    if (length > OPTIMIZED_LENGTH) {
+                        // let's resize the image
+                        float ratio = (float) w / h;
+                        int newW, newH = 0;
+
+                        if (ratio > 1.0) {
+                            newW = OPTIMIZED_LENGTH;
+                            newH = (int) (OPTIMIZED_LENGTH / ratio);
+                        } else {
+                            newH = OPTIMIZED_LENGTH;
+                            newW = (int) (OPTIMIZED_LENGTH * ratio);
+                        }
+                        mCurrentSelectedBitmap = rescaleBitmap(mCurrentSelectedBitmap, newW, newH);
+                    }
+                    // let save the new image to our local folder
+                    mImageFullPathAndName = SaveImage(mCurrentSelectedBitmap);
+                    callHODAPI();
+                }
+            }
+        }
     }
 }
